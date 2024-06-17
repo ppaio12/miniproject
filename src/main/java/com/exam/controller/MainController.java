a6a1d5dd24e3b48daec0eee702adb35176dc2039
Ginoysos12 <ppaio1997@gmail.com>
2024-06-17 12:54:45 +0900

    jw 브랜치 테스트

--- src/main/java/com/exam/controller/MainController.java
+++ src/main/java/com/exam/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
-    // jw 브랜치 테스트
+    // jw 브랜치 테스트 12:54
     @GetMapping("/main")
     public String main() {
         return "main";
