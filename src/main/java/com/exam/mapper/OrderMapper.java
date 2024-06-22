4503435f3a4b69ecfcdc1a4588befb8c3f46134e
Ginoysos12 <ppaio1997@gmail.com>
2024-06-22 17:24:53 +0900

    주문, 결제 진행중

--- src/main/java/com/exam/mapper/OrderMapper.java
+++ src/main/java/com/exam/mapper/OrderMapper.java
@@ -1,8 +1,10 @@
 package com.exam.mapper;
 
+import com.exam.dto.OrderInfoDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface OrderMapper {
 
+    int addOrder(OrderInfoDTO orderInfoDTO);
 }
